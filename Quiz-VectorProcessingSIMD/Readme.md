438a9fe90534e1f04685344298d882c94e8fd298
Jonathana1106 <jonathana1196@gmail.com>
2023-10-18 00:08:59 -0600

    [ADD] quiz SIMD

---
+++ Quiz-VectorProcessingSIMD/Readme.md
@@ -0,0 +1,36 @@
+# Instrucciones para Compilar y Ejecutar los Programas
+
+A continuación, se proporcionan las instrucciones para compilar y ejecutar los programas `pregunta3.1.cpp` y `pregunta3.2.cpp` utilizando el archivo Makefile.
+
+## Compilación
+
+Antes de ejecutar los programas, asegúrate de haber compilado los ejecutables utilizando el Makefile. Para hacerlo, sigue estos pasos:
+
+Paso 1: Abre una terminal en el directorio donde se encuentran los archivos fuente.
+Paso 2: Ejecuta el siguiente comando para compilar ambos programas:
+
+```bash
+
+make
+```
+
+## Ejecución de Binarios
+
+Una vez que hayas compilado los programas, puedes ejecutarlos de la siguiente manera:
+
+### Programa pregunta3.1
+
+Para ejecutar el programa `./pregunta3.1`, utiliza el siguiente comando:
+
+```bash
+./pregunta3.1
+```
+
+
+### Programa pregunta3.2
+
+Para ejecutar el programa `./pregunta3.2`, utiliza el siguiente comando:
+
+```bash
+./pregunta3.2
+```
---
+++ Quiz-VectorProcessingSIMD/pregunta3.2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <immintrin.h> // Encabezado necesario para intrínsecos SSE
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+    // Tamaño de los vectores, debe ser un múltiplo de 4 y mayor que 500
+    const int N = 1000;
+
+    std::vector<float> arr1(N);
+    std::vector<float> arr2(N);
+
+    // Inicializa los arreglos con valores aleatorios
+    srand(static_cast<unsigned>(time(nullptr))); // Inicializa el generador de números aleatorios
+
+    for (int i = 0; i < N; i++) {
+        // Genera valores entre 0.0 y 1.0
+        arr1[i] = static_cast<float>(rand()) / RAND_MAX;
+        arr2[i] = static_cast<float>(rand()) / RAND_MAX;
+    }
+
+    float dotProduct = 0;
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    // Vectorización con intrínsecos SSE
+    __m128 sum = _mm_setzero_ps(); // Inicializa un registro SSE con ceros
+
+    for (int i = 0; i < N; i += 4) {
+        __m128 a = _mm_loadu_ps(&arr1[i]); // Carga 4 elementos de arr1 en un registro SSE
+        __m128 b = _mm_loadu_ps(&arr2[i]); // Carga 4 elementos de arr2 en otro registro SSE
+        __m128 product = _mm_mul_ps(a, b); // Realiza la multiplicación de los registros SSE
+        sum = _mm_add_ps(sum, product); // Acumula los resultados en el registro 'sum'
+    }
+
+    // Suma horizontal de los elementos en el registro SSE
+    float result[4];
+    _mm_store_ps(result, sum);
+    dotProduct = result[0] + result[1] + result[2] + result[3];
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+
+    // Imprime el resultado del producto punto y el tiempo de ejecución con vectorización SSE
+    std::cout << "Resultado del producto punto: " << dotProduct << std::endl;
+    std::cout << "Tiempo de ejecución con vectorización SSE: " << elapsed.count() << " segundos" << std::endl;
+
+    return 0;
+}
